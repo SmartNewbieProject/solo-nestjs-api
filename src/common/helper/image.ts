08db1c00669982b836e8661e4a0e53c38434b0c3
galaxy4276 <deveungi@gmail.com>
2025-04-29 00:42:14 +0900

    feat: 헬퍼 함수 작성

---
+++ src/common/helper/image.ts
@@ -0,0 +1,27 @@
+const S3_BASE_URL = 'https://sometimes-resources.s3.ap-northeast-2.amazonaws.com/resources';
+
+
+export enum ImageResources {
+  TICKET = `${S3_BASE_URL}/ticket.png`,
+  SMS_CHECK = `${S3_BASE_URL}/sms.png`,
+  HIGH = `${S3_BASE_URL}/high.png`,
+  NOTHING = `${S3_BASE_URL}/nothing.png`,
+  SAME = `${S3_BASE_URL}/same.png`,
+  UNDER = `${S3_BASE_URL}/under.png`,
+  TERMS = `${S3_BASE_URL}/terms.png`,
+  TIME_CARD_BG = `${S3_BASE_URL}/time-card-bg.png`,
+  UNIVERSITY = `${S3_BASE_URL}/university.png`,
+  DETAILS = `${S3_BASE_URL}/details.png`,
+  IMAGE = `${S3_BASE_URL}/image.png`,
+  PAPER_PLANE = `${S3_BASE_URL}/paper-plane.png`,
+  PAPER_PLANE_WITHOUT_BG = `${S3_BASE_URL}/paper-plane-vanila.png`,
+  UNIV_BADGE = `${S3_BASE_URL}/univ-badge.png`,
+  BROKEN_SANDTIMER = `${S3_BASE_URL}/broken.png`,
+  SANDTIMER = `${S3_BASE_URL}/sand-clock.png`,
+  HEART = `${S3_BASE_URL}/heart.png`,
+  INSTAGRAM = `${S3_BASE_URL}/instagram.png`,
+  FIRE = `${S3_BASE_URL}/fire.png`,
+  LOVE_LETTER = `${S3_BASE_URL}/love-letter.png`,
+  REALTIME = `${S3_BASE_URL}/realtime.png`,
+  REVIEW = `${S3_BASE_URL}/review.png`,
+}
--- src/common/helper/index.ts
+++ src/common/helper/index.ts
@@ -1,2 +1,4 @@
 export * from './day';
 export * from './axios';
+export * from './image';
+export * from './pagination';
