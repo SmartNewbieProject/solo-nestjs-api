95358b2a209b19fc920a3b5c2041175de80a376b
galaxy4276 <deveungi@gmail.com>
2025-05-19 13:28:37 +0900

    fix: 이상한 문자 수정

--- src/database/schema/articles.ts
+++ src/database/schema/articles.ts
@@ -1,4 +1,4 @@
-wimport { pgTable, varchar, integer, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, varchar, integer, timestamp } from "drizzle-orm/pg-core";
 import { uuid, timestamps } from "./helper";
 import { users } from "./users";
 import { articleCategory } from "./article_categories";
