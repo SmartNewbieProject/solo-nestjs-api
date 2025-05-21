fec31da78e576bcad2ef6fb7fcacf01a6de18d4d
galaxy4276 <deveungi@gmail.com>
2025-05-21 22:32:21 +0900

    feat: 데이터베이스 변경사항 및 마이그레이션 추가

--- src/database/schema/profiles.ts
+++ src/database/schema/profiles.ts
@@ -18,12 +18,11 @@ export const profiles = pgTable('profiles', {
   gender: varchar('gender', { length: 10 }).$type<Gender>().notNull(),
   name: varchar('name', { length: 15 }).notNull(),
   title: varchar('title', { length: 100 }),
+  mbti: varchar('mbti', { length: 4 }),
   instagramId: varchar('instagram_id', { length: 100 }),
   is_matching_enable: boolean('is_matching_enable').default(true).notNull(),
   introduction: varchar('introduction', { length: 255 }),
-  statusAt: varchar('status_at', { length: 36 }),
   rank: varchar('rank', { length: 7, enum: ['S', 'A', 'B', 'C', 'UNKNOWN'] }).default('UNKNOWN').notNull(),
   universityDetailId: varchar('university_detail_id', { length: 36 }),
-  // appearanceGrade 필드는 더 이상 사용하지 않고 rank 필드를 사용합니다
   ...timestamps,
 });
