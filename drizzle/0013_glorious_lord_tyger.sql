3950bcdad60dde62b78a35544d5137ae3263b07c
YUJAEYUN <yujeayoon13@naver.com>
2025-07-09 12:42:09 +0900

    fix: 테이블 구조 변경으로 마이그레이션 파일 삭제

--- drizzle/0013_glorious_lord_tyger.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "users" ALTER COLUMN "email" DROP NOT NULL;
-ALTER TABLE "users" ALTER COLUMN "password" DROP NOT NULL;
-ALTER TABLE "profiles" ADD COLUMN "status_at" varchar(16);
-ALTER TABLE "users" ADD COLUMN "birthday" varchar(10);
-ALTER TABLE "users" ADD COLUMN "email_verified_at" timestamp with time zone;
